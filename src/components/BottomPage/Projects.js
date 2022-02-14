d0f07af25031682fd10437be7706c95d18af5819
William Botbyl <billybotbyl81@yahoo.com>
2022-02-13 21:04:39 -0500

    fixed link issue

--- src/components/BottomPage/Projects.js
+++ src/components/BottomPage/Projects.js
@@ -15,7 +15,7 @@ import { IconContext } from "react-icons";
 function Projects() {
   const calender = "https://github.com/WilliamBotbyl/Java-Calender";
   const calculator = "https://github.com/WilliamBotbyl/Tip-Calculator-Java";
-  const portfolio = "https://github.com/WilliamBotbyl/roughDraftPortfolio";
+  const portfolio = "https://github.com/WilliamBotbyl/Portfolio/tree/master";
   const sudokoSolver = "https://github.com/WilliamBotbyl/Sudoko-Solver";
   const seeMore = "https://github.com/WilliamBotbyl";
 
